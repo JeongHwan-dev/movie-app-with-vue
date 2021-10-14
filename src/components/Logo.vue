29b46a2e87580a65ff5d2a7ee6aa459475de229d
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-10-14 21:04:47 +0900

    Feat : 네미게이션 바 구현

---
+++ src/components/Logo.vue
@@ -0,0 +1,26 @@
+<template>
+  <RouterLink
+    to="/"
+    class="logo">
+    <span>OMDbAPI</span>.COM
+  </RouterLink>
+</template>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+
+  .logo {
+    color: $black;
+    font-family: "Oswald", sans-serif;
+    font-size: 20px;
+    text-decoration: none; 
+
+    &:hover {
+      color: $black;
+    }
+    
+    span {
+      color: $primary;
+    }
+  }
+</style>

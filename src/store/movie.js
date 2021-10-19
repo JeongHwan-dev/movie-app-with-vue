e7f2b97fd3474dd57560fefb97cca3f5339f10fd
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-10-19 19:16:55 +0900

    Refactor : search 기능 코드 리팩터링

--- src/store/movie.js
+++ src/store/movie.js
@@ -21,35 +21,64 @@ export default {
   },
   actions: {
     async searchMovies({ state, commit }, payload) {
-      const {title, type, number, year} = payload; 
-      const OMDB_API_KEY = '6a8bb20a';
-      const response = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`);
-      const { Search, totalResults } = response.data;
-
-      commit('updateState', {
-        movies: _uniqBy(Search, 'imdbID')
-      });
-
-      const total = parseInt(totalResults, 10);
-      const pageLength = Math.ceil(total / 10);
-
-      if (pageLength > 1) {
-        for (let page = 2; page <= pageLength; page++) {
-          if (page > number / 10) {
-            break;
+      try {
+        const response = await _fetchMovies({
+          ...payload,
+          page: 1
+        });
+        const { Search, totalResults } = response.data;
+  
+        commit('updateState', {
+          movies: _uniqBy(Search, 'imdbID')
+        });
+  
+        const total = parseInt(totalResults, 10);
+        const pageLength = Math.ceil(total / 10);
+  
+        if (pageLength > 1) {
+          for (let page = 2; page <= pageLength; page++) {
+            if (page > (payload.number / 10)) {
+              break;
+            }
+  
+            const response = await _fetchMovies({
+              ...payload,
+              page
+            });
+            const { Search } = response.data;
+  
+            commit('updateState', {
+              movies: [
+                ...state.movies, 
+                ..._uniqBy(Search, 'imdbID')
+              ]
+            });
           }
-
-          const response = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`);
-          const { Search } = response.data;
-
-          commit('updateState', {
-            movies: [
-              ...state.movies, 
-              ..._uniqBy(Search, 'imdbID')
-            ]
-          });
         }
+      } catch (message) {
+        commit('updateState', {
+          movies: [],
+          message
+        });
       }
     }
   }
 };
+
+function _fetchMovies(payload) {
+  const { title, type, year, page } = payload;
+  const OMDB_API_KEY = '6a8bb20a';
+  const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`;
+
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then((response) => { 
+        if (response.data.Error) {
+          reject(response.data.Error);
+        }
+
+        resolve(response);
+      })
+      .catch((error) => reject(error.message));
+  });
+}

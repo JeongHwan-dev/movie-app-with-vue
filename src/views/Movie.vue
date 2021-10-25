2174bad4506504018b12d960ef67a8c0934206ae
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-10-25 15:34:21 +0900

    Fix : 영화 평점 관련 이미지 파일 경로 오류 수정

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -57,7 +57,7 @@
               :title="name"
               class="rating">
               <img
-                :src="`/static/images/${name}.png`"
+                :src="`../assets/images/${name}.png`"
                 :alt="name"
                 class="rating__logo" />
               <span class="rating__score">{{ score }}</span>

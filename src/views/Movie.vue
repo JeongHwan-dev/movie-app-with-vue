7a8da5981f83effee88ba39f6d2884a7f27530a3
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-10-25 18:57:35 +0900

    Fix : 영화 정보 페이지의 첨부 이미지 경로 오류 해결

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -57,7 +57,7 @@
               :title="name"
               class="rating">
               <img
-                :src="`../assets/images/${name}.png`"
+                :src="`/src/assets/images/${name}.png`"
                 :alt="name"
                 class="rating__logo" />
               <span class="rating__score">{{ score }}</span>
